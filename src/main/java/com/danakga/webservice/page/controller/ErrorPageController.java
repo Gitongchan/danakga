f4ca534924084a4026a2c0e376b94d849374fd33
Jinmo <wlsah0814@naver.com>
2022-03-21 14:51:05 +0900

    에러 페이지 컨트롤러 작성 #10

--- src/main/java/com/danakga/webservice/page/controller/ErrorPageController.java
+++ src/main/java/com/danakga/webservice/page/controller/ErrorPageController.java
@@ -1,4 +1,14 @@
 package com.danakga.webservice.page.controller;
 
-public class ErrorPageController {
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ErrorPageController implements ErrorController {
+
+    @GetMapping("/error404")
+    public String error404() {
+        return "errors/404";
+    }
 }
