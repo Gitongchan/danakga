b87f9d063f882ce29df4aae52e99dfcea57474a5
wlsah0814 <71693517+wlsah0814@users.noreply.github.com>
2022-03-23 15:07:37 +0900

    Feature/10 errorcontroller (#12)
    * 에러 페이지 컨트롤러 작성 #10
    
    * 에러 페이지 컨트롤러 작성, 스프링 시큐리티 기본 로그인 해제 #10
    
    * image 오타
    
    변경완료
    
    Co-authored-by: Jinny <71691821+ByJin98@users.noreply.github.com>

--- src/main/java/com/danakga/webservice/config/WebSecurityConfig.java
+++ src/main/java/com/danakga/webservice/config/WebSecurityConfig.java
@@ -2,10 +2,19 @@ package com.danakga.webservice.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //security 기본 로그인 해제
+    @Override
+    protected void configure(HttpSecurity security) throws Exception {
+        security.httpBasic().disable();
+    }
+
 }
