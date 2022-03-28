97edb9ffcbc457fbccc723443246d97b73183680
Jung Hyeon <71696834+supark0206@users.noreply.github.com>
2022-03-28 13:20:07 +0900

    Feature/20 login (#24)
    * 중복검사 진행중 #20
    
    * 중복체크 완료 #20
    
    * 로그인 완료
    csrf 토큰 사용
    #20
    
    Co-authored-by: wjdgus43993 <71696834+wjdgus43993@users.noreply.github.com>

--- src/main/java/com/danakga/webservice/user/service/Impl/UserServiceImpl.java
+++ src/main/java/com/danakga/webservice/user/service/Impl/UserServiceImpl.java
@@ -1,32 +1,35 @@
 package com.danakga.webservice.user.service.Impl;
 
 import com.danakga.webservice.user.dto.request.UserJoinDto;
+import com.danakga.webservice.user.dto.response.ResDupliCheckDto;
 import com.danakga.webservice.user.dto.response.ResUserJoinDto;
 import com.danakga.webservice.user.model.UserInfo;
 import com.danakga.webservice.user.repository.UserRepository;
 import com.danakga.webservice.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.validation.Errors;
-import org.springframework.validation.FieldError;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 
+    @Override
+    public UserDetails loadUserByUsername(String userid) throws UsernameNotFoundException {
+        return userRepository.findByUserid(userid)
+                .orElseThrow(() -> new UsernameNotFoundException((userid)));
+    }
 
     @Autowired private final UserRepository userRepository;
-    @Autowired private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Override
     public ResUserJoinDto join(UserJoinDto userJoinDto) {
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
         String rawPassword = userJoinDto.getPassword();
-        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        userJoinDto.setPassword(bCryptPasswordEncoder.encode(rawPassword));
 
         //임시로 권한 USER로 지정
         userJoinDto.setRole("ROLE_USER");
@@ -34,8 +37,8 @@ public class UserServiceImpl implements UserService {
         final Long id = userRepository.save(
                 UserInfo.builder()
                         .userid(userJoinDto.getUserid())
-                        .password(encPassword)
-                        .username(userJoinDto.getUsername())
+                        .password(userJoinDto.getPassword())
+                        .name(userJoinDto.getName())
                         .phone(userJoinDto.getPhone())
                         .email(userJoinDto.getEmail())
                         .role(userJoinDto.getRole())
@@ -43,6 +46,26 @@ public class UserServiceImpl implements UserService {
         ).getId();
         return new ResUserJoinDto(id);
     }
+
+    @Override
+    public ResDupliCheckDto userIdCheck(String userid) {
+        //.isPresent , Optional객체가 있으면 true null이면 false 반환
+      if(userRepository.findByUserid(userid).isPresent()){
+       return new ResDupliCheckDto(-1); //같은 userid있으면 -1반환
+      }
+      return new ResDupliCheckDto(1);
+    }
+
+    @Override
+    public ResDupliCheckDto emailCheck(String email) {
+        if(userRepository.findByEmail(email).isPresent()){
+            System.out.println("같은이메일이 존재함");
+            return new ResDupliCheckDto(-1);
+        }
+        System.out.println("같은 이메일이 없다");
+        return new ResDupliCheckDto(1);
+    }
+
 
 
 }
