0c02def21a863801b15c33a65f992d985433d6df
wlsah0814 <wlsah0814@naver.com>
2022-05-10 21:32:23 +0900

    파일 업로드 확장자 제한 후에도 게시글 작성되는 오류 확인 (파일은 업로드 되지 않고 게시글만 작성 됨), 수정 중 #55

--- src/main/java/com/danakga/webservice/board/service/Impl/FileServiceImpl.java
+++ src/main/java/com/danakga/webservice/board/service/Impl/FileServiceImpl.java
@@ -9,7 +9,6 @@ import com.danakga.webservice.board.service.FilesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -27,7 +26,6 @@ public class FileServiceImpl implements FilesService {
     public ResFileUploadDto saveFileUpload(ReqFileUploadDto reqFileUploadDto, List<MultipartFile> files, Board board) {
 
             Long f_id = null;
-
             //파일 저장 경로
             String savepath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files\\";
 
@@ -44,53 +42,43 @@ public class FileServiceImpl implements FilesService {
             // multipartfile : files  files에서 더 꺼낼게 없을 때 까지 multipartfile에 담아줌
             for(MultipartFile multipartFile : files) {
 
-                //contenttype이 multipart/form-data로
-                String originFileExtension;
-                String originFileName = multipartFile.getOriginalFilename();
-                String contentType = multipartFile.getContentType();
+                //파일명 소문자로 추출
+                String originFileName = multipartFile.getOriginalFilename().toLowerCase();
 
-                //확장자명 없으면 처리 x, jpg, png 두개만 처리, ObjectUtils.isEmpty 객체가 있는지 없는지 true false로 반환
-                //contains로 ()안에 문자열이 ContentType 변수안에 있는지 확인 후 originFileExtension에 해당되는 문자열 담아줌
-//                if(ObjectUtils.isEmpty(contentType)) {
-//                    break;
-//                } else {
-//                    if(contentType.contains("image/jpeg"))
-//                        originFileExtension = ".jpeg";
-//                    else if(contentType.contains("image/png"))
-//                        originFileExtension = ".png";
-//                    else if(contentType.contains("image/jpg"))
-//                        originFileExtension = ".jpg";
-//                    else
-//                        break;
-//                }
-                
-                //UUID로 파일명 중복되지 않게 유일한 식별자 + 확장자로 저장
-                UUID uuid = UUID.randomUUID();
-                String saveFileName = uuid + "__" + originFileName;
+                //확장자 제한
+                if(originFileName.endsWith(".jpg") || originFileName.endsWith(".png") || originFileName.endsWith(".jpeg")) {
 
-                //File로 저장 경로와 저장 할 파일명 합쳐서 transferTo() 사용하여 업로드하려는 파일을 해당 경로로 저장
-                String filepath = savepath + "\\" + saveFileName;
-                try {
-                    multipartFile.transferTo(new File(filepath));
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    //UUID로 파일명 중복되지 않게 유일한 식별자 + 확장자로 저장
+                    UUID uuid = UUID.randomUUID();
+                    String saveFileName = uuid + "__" + originFileName;
+
+                    //File로 저장 경로와 저장 할 파일명 합쳐서 transferTo() 사용하여 업로드하려는 파일을 해당 경로로 저장
+                    String filepath = savepath + "\\" + saveFileName;
+                    try {
+                        multipartFile.transferTo(new File(filepath));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    //설정한 파일명, 경로 값 넘겨주기
+                    reqFileUploadDto.setF_originName(originFileName);
+                    reqFileUploadDto.setF_savename(saveFileName);
+                    reqFileUploadDto.setF_path(filepath);
+
+                    //board의 id값 들어가는거 확인
+                    //나머지 파일 관련 값은 안들어감
+                    f_id = fileRepository.save(
+                            Files.builder()
+                                    .f_savename(reqFileUploadDto.getF_savename())
+                                    .f_origin(reqFileUploadDto.getF_originName())
+                                    .f_path(reqFileUploadDto.getF_path())
+                                    .board(board)
+                                    .build()
+                    ).getF_id();
+                } else {
+                    System.out.println("jpg, png, jpeg 형식의 파일만 업로드 가능합니다.");
+                    break;
                 }
-
-                //set으로 설정한 파일명, 경로 값 넘겨주기
-                reqFileUploadDto.setF_originName(multipartFile.getOriginalFilename());
-                reqFileUploadDto.setF_savename(saveFileName);
-                reqFileUploadDto.setF_path(filepath);
-
-                //board의 id값 들어가는거 확인
-                //나머지 파일 관련 값은 안들어감
-                 f_id = fileRepository.save(
-                        Files.builder()
-                                .f_savename(reqFileUploadDto.getF_savename())
-                                .f_origin(reqFileUploadDto.getF_originName())
-                                .f_path(reqFileUploadDto.getF_path())
-                                .board(board)
-                                .build()
-                ).getF_id();
             }
         return new ResFileUploadDto(f_id);
     }
