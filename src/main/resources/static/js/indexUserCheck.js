263a6503dab218077655014c8f4e8591969caa9c
Jinny <ppwm111@naver.com>
2022-05-18 20:41:34 +0900

    로그인 시 index이동 구현중 #90

--- src/main/resources/static/js/indexUserCheck.js
+++ src/main/resources/static/js/indexUserCheck.js
@@ -1,28 +1,72 @@
 //토근값 가져오기
 const token = document.querySelector('meta[name="_csrf"]').content;
 
-//ID넣는 곳
-const span_id = document.querySelector('#text_id');
+const span_id = document.createElement('span');
+const login_info = document.querySelector('.userinfo');
+const div_id = document.querySelector('.view_id');
 
-const user_login = document.querySelector('.middle-right-area._user')
-const not_login = document.querySelector('.middle-right-area._noInfo')
 //로그인 안되어있다면 로그인, 회원가입 > 로그인 되어있다면 내정보|로그아웃
 
-const userDataCheck = async ()=> {
-    alert("첫번째 실행");
+const left_a = document.createElement('a');
+const left_span = document.createElement('span');
+const right_a = document.createElement('a');
+const right_span = document.createElement('span');
+
+//
+const stick = document.createElement('span');
+stick.innerText = '|';
+
+const logout_form = document.createElement('form');
+const hidden_token = document.createElement('input');
+const logout_btn = document.createElement('button');
+
+async function userDataCheck(){
     const res = await fetch('/api/user')
-    alert("두번째 실행");
-
-    const data = await res.json();
-    alert("세번째 실행");
-
-    console.log(res);
-    console.log(data);
-    not_login.style.display = "flex";
-    span_id.innerText = `${data.userid}님! 안녕하세요!`;
-    
+        .then((response)=>response.json())
+        .then((data)=> {return data})
+    return {res};
 
 }
+window.addEventListener('DOMContentLoaded', async ()=>{
+    const res = await fetch('/api/user')
+    const data = await res.json();
+    console.log(res);
+    console.log(data);
+    if(res.status === 200){
+        span_id.innerText = `${data.userid}님! 안녕하세요!`;
 
-console.log(userDataCheck);
-window.addEventListener('DOMContentloaded',userDataCheck);
+        left_a.href = '/mypage';
+        left_span.innerText = '내 정보';
+        left_a.appendChild(left_span);
+
+        // right_a.href = '/user/logout';
+        // right_span.innerText = "로그아웃"
+        // right_a.appendChild(right_span);
+        hidden_token.type = 'hidden';
+        hidden_token.name = '_csrf';
+        hidden_token.value = token;
+        logout_btn.innerText = '로그아웃';
+
+        logout_form.method = 'POST';
+        logout_form.action = '/user/logout';
+        logout_form.appendChild(hidden_token);
+        logout_form.appendChild(logout_btn);
+
+        div_id.appendChild(span_id);
+        login_info.appendChild(left_a);
+        login_info.appendChild(logout_form);
+    }else if(res.status !== 200){
+        span_id.innerText = `로그인이 필요합니다!`;
+        left_a.href = '/login';
+        left_span.innerText = '로그인';
+        left_a.appendChild(left_span);
+
+        right_a.href = '/register';
+        right_span.innerText = '회원가입';
+        right_a.appendChild(right_span);
+
+        div_id.appendChild(span_id);
+        login_info.appendChild(left_a);
+        login_info.appendChild(right_a);
+    }
+});
