f3936c7a6388329c6058485fb8961b682a5c7204
Jinmo <71693517+wlsah0814@users.noreply.github.com>
2022-05-26 18:21:07 +0900

    Feature/105 게시글 조회 수, 게시판 구분  (#109)
    * 게시글 조회 수 쿠키로 조회 수 중복 검증 진행 중 #105
    
    * 게시글 조회 수 중복 검증 진행 중, 게시판 구분 완료 #105
    
    * 게시글 조회 수 중복 증가 완료, 게시글 작성 @RequestPart 키 값 작성 #105

--- src/main/java/com/danakga/webservice/board/service/Impl/BoardServiceImpl.java
+++ src/main/java/com/danakga/webservice/board/service/Impl/BoardServiceImpl.java
@@ -21,6 +21,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.*;
 
 @Service
@@ -35,12 +38,12 @@ public class BoardServiceImpl implements BoardService {
 
     //게시판 목록
     @Override
-    public List<ResBoardListDto> boardList(Pageable pageable) {
+    public List<ResBoardListDto> boardList(Pageable pageable, String board_type) {
 
         //deleted 컬럼에 N값인 컬럼만 모두 List에 담아줌
         final String deleted = "N";
         pageable = PageRequest.of(0, 5, Sort.by("bdId").descending());
-        List<Board> boards = boardRepository.findAllByBdDeleted(deleted, pageable).getContent();
+        List<Board> boards = boardRepository.findAllByBdDeletedAndBdType(deleted, board_type, pageable).getContent();
 
         List<ResBoardListDto> boardListDto = new ArrayList<>();
 
@@ -59,7 +62,7 @@ public class BoardServiceImpl implements BoardService {
 
     //게시글 조회
     @Override
-    public ResBoardPostDto getpost(Long id) {
+    public ResBoardPostDto getpost(Long id, HttpServletRequest request, HttpServletResponse response) {
 
         Optional<Board> boardWrapper = boardRepository.findById(id);
         
@@ -71,33 +74,69 @@ public class BoardServiceImpl implements BoardService {
         Board board = boardWrapper.get();
         List<Board_Files> files = fileRepository.findByBoard(board);
 
+        //조회수 증가, 쿠키로 중복 증가 방지
+        //쿠키가 있으면 그 쿠키가 해당 게시글 쿠키인지 확인하고 아니라면 조회수 올리고 setvalue로 해당 게시글의 쿠키 값도 넣어줘야함
+        Cookie oldCookie = null;
+        Cookie[] cookies = request.getCookies();
+
+        //기존에 쿠키를 가지고 있다면 해당 쿠키를 oldCookie에 담아줌
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("postView")) {
+                    oldCookie = cookie;
+                }
+            }
+        }
+
+        //oldCookie가 쿠키를 가지고 있으면 oldCookie의 value값에 id가 없다면 조회수 증가
+        //그리고 해당 게시글 id에 대한 쿠키를 다시 담아서 보냄
+        if(oldCookie != null) {
+            if(!oldCookie.getValue().contains("[" + id.toString() + "]")) {
+                boardRepository.updateView(id);
+                oldCookie.setValue(oldCookie.getValue() + "[" + id + "]");
+                oldCookie.setPath("/");
+                oldCookie.setMaxAge(60 * 60);
+                response.addCookie(oldCookie);
+            }
+        } else {
+            boardRepository.updateView(id);
+            Cookie postCookie = new Cookie("postView", "[" + id + "]");
+            postCookie.setPath("/");
+            //쿠키 사용시간 1시간 설정
+            postCookie.setMaxAge(60 * 60);
+            System.out.println("쿠키 이름 : " + postCookie.getValue());
+            response.addCookie(postCookie);
+        }
+
+
+
         //Map을 List에 넣어서 여러개를 받을 수 있게 함
         List<Map<String, Object>> mapFiles = new ArrayList<>();
-        List<ResBoardPostDto> postDto = new ArrayList<>();
+        ResBoardPostDto resBoardPostDto = new ResBoardPostDto();
 
         //개별 게시글 값 set
-        ResBoardPostDto resBoardPostDto = new ResBoardPostDto();
-        resBoardPostDto.setBdId(board.getBdId());
-        resBoardPostDto.setBdWriter(board.getBdWriter());
-        resBoardPostDto.setBdTitle(board.getBdTitle());
-        resBoardPostDto.setBdContent(board.getBdContent());
-        resBoardPostDto.setBdCreated(board.getBdCreated());
-        resBoardPostDto.setBdModified(board.getBdModified());
-        resBoardPostDto.setBdViews(board.getBdViews());
-        postDto.add(resBoardPostDto);
+            resBoardPostDto.setBdId(board.getBdId());
+            resBoardPostDto.setBdWriter(board.getBdWriter());
+            resBoardPostDto.setBdTitle(board.getBdTitle());
+            resBoardPostDto.setBdContent(board.getBdContent());
+            resBoardPostDto.setBdCreated(board.getBdCreated());
+            resBoardPostDto.setBdModified(board.getBdModified());
+            resBoardPostDto.setBdViews(board.getBdViews());
 
         //file 정보 값 set
         //Map의 put은 키값마다 1개씩만 담기기 때문에 map 생성자를 밖으로 빼면 가장 마지막으로 들어온 값만 저장됨 (결국 1개만 저장)
         //그래서 map 생성자도 반복문 안으로 넣어줘서 List<Map>에 한번 담고 다시 생성돼서 돌아가는 식
         //Map.put() == List.add() 와 같은 기능
         //Map에 담긴 값을 Dto에 선언했던 Lise<Map<?,?>>에 담아줌
-        files.forEach(entity -> {
-            Map<String, Object> filesmap = new HashMap<>();
-            filesmap.put("file_name",entity.getFSavename());
-            filesmap.put("file_path",entity.getFPath());
-            mapFiles.add(filesmap);
-            resBoardPostDto.setFiles(mapFiles);
-        });
+        //for(Board_Files board_files : files) {} 으로도 가능
+            files.forEach(entity -> {
+                Map<String, Object> filesmap = new HashMap<>();
+                filesmap.put("file_name",entity.getFSavename());
+                filesmap.put("file_path",entity.getFPath());
+                mapFiles.add(filesmap);
+            });
+        resBoardPostDto.setFiles(mapFiles);
+
         return resBoardPostDto;
     }
 
@@ -115,7 +154,6 @@ public class BoardServiceImpl implements BoardService {
             boardRepository.save(
                     board = Board.builder()
                             .bdType(reqBoardWriteDto.getBdType())
-                            .bdViews(reqBoardWriteDto.getBdViews())
                             .bdWriter(recentUserInfo.getUserid())
                             .bdTitle(reqBoardWriteDto.getBdTitle())
                             .bdContent(reqBoardWriteDto.getBdContent())
@@ -136,7 +174,6 @@ public class BoardServiceImpl implements BoardService {
                         boardRepository.save(
                                 board = Board.builder()
                                         .bdType(reqBoardWriteDto.getBdType())
-                                        .bdViews(reqBoardWriteDto.getBdViews())
                                         .bdWriter(recentUserInfo.getUserid())
                                         .bdTitle(reqBoardWriteDto.getBdTitle())
                                         .bdContent(reqBoardWriteDto.getBdContent())
