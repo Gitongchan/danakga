988f313458db7246d4dc9d428f32fcb9666d70cb
Jung Hyeon <71696834+supark0206@users.noreply.github.com>
2022-05-27 11:42:10 +0900

    Feature/101 product file upload (#111)
    * 상품 이미지 파일 등록 완료 #101
    
    * 상품 업로드 productDto RequestPart value 추가 #101
    
    * 상품 썸네일 추가 완료 #101

--- src/main/java/com/danakga/webservice/product/service/ProductFilesService.java
+++ src/main/java/com/danakga/webservice/product/service/ProductFilesService.java
@@ -1,4 +1,10 @@
 package com.danakga.webservice.product.service;
 
+import com.danakga.webservice.product.model.Product;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
 public interface ProductFilesService {
+    Long uploadFile(List<MultipartFile> files, Product product);
 }
