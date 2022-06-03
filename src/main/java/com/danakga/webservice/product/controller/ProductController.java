f02d3158cb05f71470b29ec4464b4bba06a1814e
supark0206 <supark0206@gmail.com>
2022-06-03 10:32:31 +0900

    상품 리스트 페이징 jpa 완료#121

--- src/main/java/com/danakga/webservice/product/controller/ProductController.java
+++ src/main/java/com/danakga/webservice/product/controller/ProductController.java
@@ -46,8 +46,8 @@ public class ProductController {
     //상품 리스트
     @GetMapping("/list")
     public List<ResProductListDto> productList(Pageable pageable,
-                                               @RequestBody ProductSearchDto productSearchDto, int page) {
-        return null;
+                                               @RequestPart(value = "productSearchDto") ProductSearchDto productSearchDto, int page) {
+        return productService.productList(pageable,productSearchDto,page);
     }
 
     //상품 조회
