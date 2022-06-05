5420fcecfaf8e5a5fa4f06c712bbffe51b1ed66a
wlsah0814 <wlsah0814@naver.com>
2022-06-05 13:45:36 +0900

    파일 업로드 경로 변경 #128

--- src/main/java/com/danakga/webservice/util/FilePath/ExternalPath.java
+++ src/main/java/com/danakga/webservice/util/FilePath/ExternalPath.java
@@ -1,5 +1,6 @@
 package com.danakga.webservice.util.FilePath;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class ExternalPath implements WebMvcConfigurer {
 
+    @Value("${file.path}") private String pathTest;
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("")
+                //파일 요청 경로
+        registry.addResourceHandler("http://localhost:8081/")
                 //실제 파일이 존재하는 외부 경로
-                .addResourceLocations(System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files");
+                .addResourceLocations(pathTest);
     }
 }
