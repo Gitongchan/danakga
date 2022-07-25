4e3003426fc60893ff157d4dc390153983404499
KiMByungwok <quddnrquddnr2302@gmail.com>
2022-07-25 16:10:52 +0900

    검색 기능 구현중 #191

---
+++ src/main/resources/static/js/board/boardSearch.js
@@ -0,0 +1,9 @@
+
+const btn = document.querySelector('#searchbtn');
+const search = document.querySelector('#search');
+const searchselect = document.querySelector('.form-select');
+
+
+btn.addEventListener('click',function (search){
+
+})
