931c4331b795fc95162d40ef54b0fe85b2709260
KiMByungwok <quddnrquddnr2302@gmail.com>
2022-07-25 18:55:41 +0900

    검색 기능 구현 #191

--- src/main/resources/static/js/board/boardSearch.js
+++ src/main/resources/static/js/board/boardSearch.js
@@ -1,9 +1,41 @@
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+const searchBtn = document.querySelector('#searchbtn');
+const content = document.querySelector('#search');
+const category = document.querySelector('.form-select');
+const type = getParameterByName('type');
 
-const btn = document.querySelector('#searchbtn');
-const search = document.querySelector('#search');
-const searchselect = document.querySelector('.form-select');
+const getInfo = async () => {
+    console.log(category.value);
+    if(content.value.trim().length===0){
+        alert('값을 입력해주세요!')
+        return;
+    }
+    try{
+        const res = await  fetch(`/api/board/list/search/${type}/${category.value}/${content.value}?page=0`);
+        if(res.ok){
+            const data = await res.json();
+            tablelist.innerHTML = '';
+            for(let i=0; i<data.lists.length; i++){
+                const tr = document.createElement('tr');
+                tr.innerHTML =
+                    `<td>${data.lists[i].bd_id}</td>
+                 <td><a href="/board/info?boardid=${data.lists[i].bd_id}?bdwriter=${data.lists[i].bd_writer}">${data.lists[i].bd_title}</a></td>
+                 <td>${data.lists[i].bd_writer}</td>
+                 <td>${data.lists[i].bd_created.split('.')[0]}</td>
+                 <td>${data.lists[i].bd_views}</td>`
 
+                tablelist.appendChild(tr);
+            }
+        }
+    }catch (e) {
 
-btn.addEventListener('click',function (search){
+    }
 
-})
+}
+
+searchBtn.addEventListener('click', getInfo);
