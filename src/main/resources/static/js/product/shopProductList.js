cc450ffa7639f797588e4bb4ee4f8593b82387ad
Jinny <ppwm111@naver.com>
2022-10-20 14:29:56 +0900

    feat: 매니저 내상품 조회 카테고리 나누기 완료 #290

--- src/main/resources/static/js/product/shopProductList.js
+++ src/main/resources/static/js/product/shopProductList.js
@@ -7,17 +7,39 @@
 // 다음 버튼 : 화면에 그려진 마지막 페이지 + 1
 const pagenation = document.querySelector('.product-list .text-center .pagination');
 
+// 리스트
 const list = document.querySelector('#product-list');
 
-document.getElementById('product-listBtn').addEventListener('click',async function () {
-    console.log('조회 버튼 클릭됨')
-});
+//카테고리들
+const mainType = document.getElementById('main-type');
+const subType = document.getElementById('sub-type');
+const brandType = document.querySelector('#brand-type');
+const categoryType = document.querySelector('#category-type');
+const productName = document.querySelector('#productName');
+const startPrice = document.querySelector('#startPrice');
+const endPrice = document.querySelector('#endPrice');
+const sort = document.querySelector('#sort-type');
 
+// URL 설정
 let productUrl = '';
 let paging = ''
 let productPaging = ''
 
-function setURL(startDate, endDate, productName, productStock, productType, productSubType, productBrand, startPrice, endPrice, sortBy, sortMethod, page) {
+// 날짜
+let today = new Date();
+
+
+document.getElementById('product-listBtn').addEventListener('click',async function () {
+    if(startPrice.value > endPrice.value){
+        alert("가격범위를 다시 설정해주세요!")
+        return
+    }
+    setURL(startDate, endDate, productName.value ? productName.value : '%25', 1, mainType.value, subType.value, brandType.value, startPrice.value ? startPrice.value : 0, endPrice.value ? endPrice : 9999999, categoryType.value, sort.value, 0)
+    myProductList(productPaging)
+});
+
+
+function setURL(startDate=today.toISOString().substring(0,11)+'00:00', endDate= today.toISOString().substring(0,11)+'23:59', productName='%25', productStock = 1, productType, productSubType, productBrand, startPrice=0, endPrice=9999999, sortBy, sortMethod, page) {
     productUrl = `/api/manager/product/list?startDate=${startDate}&endDate=${endDate}&productName=${productName}&productStock=${productStock}&productType=${productType}&productSubType=${productSubType}&productBrand=${productBrand}&startPrice=${startPrice}&endPrice=${endPrice}&sortBy=${sortBy}&sortMethod=${sortMethod}&`
     paging = `page=${page}`
     productPaging = productUrl + paging
@@ -30,11 +52,11 @@ async function myProductList(url){
     console.log(data);
 
     pagenation.innerHTML = "";
-    renderPagination(url, data[0].totalPage)
 
     try{
         if(res.status===200){
             list.innerHTML= ''
+            renderPagination(url, data[0].totalPage)
             for(let datalist in data) {
                 const item = data[datalist];
                 const tr = document.createElement('tr');
@@ -78,7 +100,7 @@ async function myProductList(url){
             }
         }
     }catch (e) {
-        alert("불러오기 실패!");
+        alert("정보가 없습니다!");
     }
 }
 
@@ -176,9 +198,92 @@ function onList(e){
     myProductList(productUrl + `page=${e}`)
 }
 
+//메인 타입 변경시 서브타입이 알맞게 들어가도록
+function changeSubType(){
+    const mainTypeValue = mainType.options[mainType.selectedIndex].value;
+
+    if(mainTypeValue === "바다로드"){
+        subType.innerHTML = `
+                        <option value="%25">상관없음</option>
+                        <option value="인쇼어">인쇼어</option>
+                        <option value="라이트지깅">라이트지깅</option>
+                        <option value="볼락/락피쉬">볼락/락피쉬</option>
+                        <option value="지깅로드">지깅로드</option>
+                        <option value="선상캐스트로드">선상캐스트로드</option>
+                        <option value="외수질/침선">외수질/침선</option>
+                        <option value="타이라바">타이라바</option>
+                        <option value="좌대용">좌대용</option>
+                        <option value="두족류">두족류</option>
+
+            `;
+    }else if(mainTypeValue === "민물로드"){
+        subType.innerHTML = `
+                        <option value="%25">상관없음</option>
+                        <option value="스피닝로드">스피닝로드</option>
+                        <option value="베이트로드">베이트로드</option>
+                        <option value="계류로드">계류로드</option>
+                        <option value="가물치로드">가물치로드</option>
+                        <option value="빙어로드">빙어로드</option>
+                        <option value="플라이로드">플라이로드</option>
+
+            `;
+    }else if(mainTypeValue === "원투낚시"){
+        subType.innerHTML = `
+                        <option value="%25">상관없음</option>
+                        <option value="원투로드">원투로드</option>
+                        <option value="원투릴">원투릴</option>
+                        <option value="원투채비">원투채비</option>
+                        <option value="원투소품">원투소품</option>
+            `;
+    }else if(mainTypeValue === "릴/용품"){
+        console.log('릴용품')
+        subType.innerHTML = `
+                        <option value="%25">상관없음</option>
+                        <option value="스피닝릴">스피닝릴</option>
+                        <option value="베이트릴">베이트릴</option>
+                        <option value="전동릴">전동릴</option>
+                        <option value="빙어릴">빙어릴</option>
+                        <option value="전동릴부품">전동릴부품</option>
+                        <option value="튜닝용품">튜닝용품</option>
+                        <option value="플라이">플라이</option>
+            `;
+    }else if(mainTypeValue === "라인/용품"){
+        subType.innerHTML = `
+                        <option value="%25">상관없음</option>
+                        <option value="카본라인">카본라인</option>
+                        <option value="합사라인">합사라인</option>
+                        <option value="모노라인">모노라인</option>
+                        <option value="하이브리드라인">하이브리드라인</option>
+                        <option value="플라이라인">플라이라인</option>
+                        <option value="라인결속기">라인결속기</option>
+                        <option value="플라이">플라이</option>
+            `;
+    }else if(mainTypeValue === "바늘/훅"){
+        subType.innerHTML = `
+                        <option value="%25">상관없음</option>
+                        <option value="지그헤드">지그헤드</option>
+                        <option value="다운샷">다운샷</option>
+                        <option value="와이드갭">와이드갭</option>
+                        <option value="스트레이트">스트레이트</option>
+                        <option value="플래시스위머">플래시스위머</option>
+                        <option value="웜스프링">웜스프링</option>
+                        <option value="플라이">플라이</option>
+                        <option value="타이라바">타이라바</option>
+            `;
+    }else{
+        //기타일 시
+        subType.innerHTML = `
+                      <option value="%25" selected>상관없음</option>
+
+            `;
+    }
+}
+
+
+
 // 초기 실행
 (function(){
-    setURL('2022-05-01T01:30','2100-06-14T17:30','%25',1,'%25','%25','%25','0','99999999','productUploadDate','des',0);
+    setURL('2000-06-14T17:30',today.toISOString().substring(0,11)+'23:59','%25',1,'%25','%25','%25','0','99999999','productUploadDate','des',0);
     myProductList(productPaging);
 })();
 
