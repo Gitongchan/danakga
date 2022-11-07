40bd9713e56fb7e8b2e2bbc5a381cd0c05f4d487
wlsah0814 <wlsah0814@naver.com>
2022-11-07 14:39:20 +0900

    상품 Qna로 변경 후 테스트 완료 #316

--- src/main/java/com/danakga/webservice/qna/controller/productanswer/ProductAnswerController.java
+++ src/main/java/com/danakga/webservice/qna/controller/productanswer/ProductAnswerController.java
@@ -16,33 +16,33 @@ public class ProductAnswerController {
     private final AnswerService AnswerService;
 
     /* 가게 문의사항 답변 작성 */
-    @PostMapping("/product_answer/write/{c_id}/{qn_id}")
+    @PostMapping("/product_answer/write/{p_id}/{qn_id}")
     public ResResultDto productAnswerWrite(@LoginUser UserInfo userInfo,
                                         @RequestBody ReqAnswerDto reqAnswerDto,
-                                        @PathVariable("c_id") Long c_id,
+                                        @PathVariable("p_id") Long p_id,
                                         @PathVariable("qn_id") Long qn_id) {
 
-        return AnswerService.productAnswerWrite(userInfo, reqAnswerDto, c_id, qn_id);
+        return AnswerService.productAnswerWrite(userInfo, reqAnswerDto, p_id, qn_id);
     }
     
     /* 가게 문의사항 답변 수정 */
-    @PutMapping("/product_answer/edit/{c_id}/{qn_id}/{an_id}")
+    @PutMapping("/product_answer/edit/{p_id}/{qn_id}/{an_id}")
     public ResResultDto productAnswerEdit(@LoginUser UserInfo userInfo,
                                        @RequestBody ReqAnswerDto reqAnswerDto,
-                                       @PathVariable("c_id") Long c_id,
+                                       @PathVariable("p_id") Long p_id,
                                        @PathVariable("qn_id") Long qn_id,
                                        @PathVariable("an_id") Long an_id) {
 
-        return AnswerService.productAnswerEdit(userInfo, reqAnswerDto, c_id, qn_id, an_id);
+        return AnswerService.productAnswerEdit(userInfo, reqAnswerDto, p_id, qn_id, an_id);
     }
 
     /* 가게 문의사항 답변 삭제 */
-    @PutMapping("/product_answer/delete/{c_id}/{qn_id}/{an_id}")
+    @PutMapping("/product_answer/delete/{p_id}/{qn_id}/{an_id}")
     public ResResultDto productAnswerDelete(@LoginUser UserInfo userInfo,
-                                         @PathVariable("c_id") Long c_id,
+                                         @PathVariable("p_id") Long p_id,
                                          @PathVariable("qn_id") Long qn_id,
                                          @PathVariable("an_id") Long an_id) {
         
-        return AnswerService.productAnswerDelete(userInfo, c_id, qn_id, an_id);
+        return AnswerService.productAnswerDelete(userInfo, p_id, qn_id, an_id);
     }
 }
