20797b471edd57004a678cba50c908766d875dd8
wlsah0814 <wlsah0814@naver.com>
2022-11-07 11:53:41 +0900

    변수명, 로직 변경 후 테스트 중 #316

--- src/main/java/com/danakga/webservice/qna/service/Impl/QnaServiceImpl.java
+++ src/main/java/com/danakga/webservice/qna/service/Impl/QnaServiceImpl.java
@@ -3,6 +3,8 @@ package com.danakga.webservice.qna.service.Impl;
 import com.danakga.webservice.company.model.CompanyInfo;
 import com.danakga.webservice.company.repository.CompanyRepository;
 import com.danakga.webservice.exception.CustomException;
+import com.danakga.webservice.product.model.Product;
+import com.danakga.webservice.product.repository.ProductRepository;
 import com.danakga.webservice.qna.dto.request.ReqQnaDto;
 import com.danakga.webservice.qna.dto.response.ResQnaDto;
 import com.danakga.webservice.qna.model.Qna;
@@ -31,64 +33,102 @@ public class QnaServiceImpl implements QnaService {
 
     private final UserRepository userRepository;
     private final CompanyRepository companyRepository;
+    private final ProductRepository productRepository;
     private final QnaRepository qnaRepository;
 
     /* 문의사항 목록 */
     @Override
-    public ResQnaDto qnaList(Pageable pageable, Integer q_sort, int page, Long c_id) {
+    public ResQnaDto qnaList(Pageable pageable, Integer q_sort, int page, Long p_id) {
 
         final String deleted = "N";
-
-        Page<Qna> Qna;
 
         List<Map<String, Object>> qnaList = new ArrayList<>();
 
         /* 사이트 문의사항 목록 */
-        if(c_id == null) {
+        if(p_id == null) {
             /* 들어오는 sort값 따라서 사이트, 가게 문의사항 구분 */
             pageable = PageRequest.of(page, 10, Sort.by("qnCreated").descending());
-            Qna = qnaRepository.findByQnDeletedAndQnSort(pageable, deleted, q_sort);
+            Page<Qna> checkQna = qnaRepository.findByQnDeletedAndQnSort(pageable, deleted, q_sort);
 
-            Qna.forEach(entity -> {
+            checkQna.forEach(entity -> {
 
-                Map<String,Object> qnaMap = new LinkedHashMap<>();
+                Map<String,Object> siteQnaMap = new LinkedHashMap<>();
 
-                qnaMap.put("q_id", entity.getQnId());
-                qnaMap.put("q_type", entity.getQnType());
-                qnaMap.put("q_userid", entity.getUserInfo().getUserid());
-                qnaMap.put("q_title", entity.getQnTitle());
-                qnaMap.put("q_created", entity.getQnCreated());
-                qnaMap.put("q_state", entity.getQnState());
-                qnaMap.put("totalPage", Qna.getTotalPages());
-                qnaMap.put("totalElement", Qna.getTotalPages());
+                siteQnaMap.put("qn_id", entity.getQnId());
+                siteQnaMap.put("qn_type", entity.getQnType());
+                siteQnaMap.put("qn_userid", entity.getUserInfo().getUserid());
+                siteQnaMap.put("qn_title", entity.getQnTitle());
+                siteQnaMap.put("qn_created", entity.getQnCreated());
+                siteQnaMap.put("qn_state", entity.getQnState());
+                siteQnaMap.put("totalPage", checkQna.getTotalPages());
+                siteQnaMap.put("totalElement", checkQna.getTotalPages());
 
-                qnaList.add(qnaMap);
+                qnaList.add(siteQnaMap);
             });
 
             return new ResQnaDto(qnaList);
         }
 
+        /* 상품별 문의사항 목록 */
+        Product checkProduct = productRepository.findById(p_id)
+                .orElseThrow(() -> new CustomException.ResourceNotFoundException("상품을 찾을 수 없습니다."));
+
+        CompanyInfo checkCompanyInfo = companyRepository.findById(checkProduct.getProductCompanyId().getCompanyId())
+                .orElseThrow(() -> new CustomException.ResourceNotFoundException("가게 정보를 찾을 수 없습니다."));
+
+        pageable = PageRequest.of(page, 10, Sort.by("qnCreated").descending());
+        Page<Qna> checkQna = qnaRepository.findByQnDeletedAndProduct(deleted, checkProduct, pageable);
+
+        checkQna.forEach(entity -> {
+
+            Map<String,Object> productQnaMap = new LinkedHashMap<>();
+
+            productQnaMap.put("qn_id", entity.getQnId());
+            productQnaMap.put("qn_type", entity.getQnType());
+            productQnaMap.put("qn_userid", entity.getUserInfo().getUserid());
+            productQnaMap.put("qn_title", entity.getQnTitle());
+            productQnaMap.put("qn_created", entity.getQnCreated());
+            productQnaMap.put("qn_state", entity.getQnState());
+            productQnaMap.put("product_name", entity.getProduct().getProductName());
+            productQnaMap.put("company_Info", entity.getCompanyInfo().getCompanyName());
+            productQnaMap.put("totalPage", checkQna.getTotalPages());
+            productQnaMap.put("totalElement", checkQna.getTotalPages());
+
+            qnaList.add(productQnaMap);
+
+        });
+
+        return new ResQnaDto(qnaList);
+    }
+
+    /* 가게 문의사항 전체 목록 */
+    @Override
+    public ResQnaDto companyQnaList(Pageable pageable, int page, Long c_id) {
+
         CompanyInfo checkCompanyInfo = companyRepository.findById(c_id)
                 .orElseThrow(() -> new CustomException.ResourceNotFoundException("가게 정보를 찾을 수 없습니다."));
 
-        /* 가게 문의사항 목록 */
         pageable = PageRequest.of(page, 10, Sort.by("qnCreated").descending());
-        Qna = qnaRepository.findByQnDeletedAndCompanyInfo(deleted, checkCompanyInfo, pageable);
+        Page<Qna> checkQna = qnaRepository.findByCompanyInfo(checkCompanyInfo, pageable);
 
-        Qna.forEach(entity -> {
+        List<Map<String, Object>> qnaList = new ArrayList<>();
 
-            Map<String,Object> qnaMap = new LinkedHashMap<>();
+        checkQna.forEach(entity -> {
 
-            qnaMap.put("q_id", entity.getQnId());
-            qnaMap.put("q_type", entity.getQnType());
-            qnaMap.put("q_userid", entity.getUserInfo().getUserid());
-            qnaMap.put("q_title", entity.getQnTitle());
-            qnaMap.put("q_created", entity.getQnCreated());
-            qnaMap.put("q_state", entity.getQnState());
-            qnaMap.put("companyInfo", entity.getCompanyInfo().getCompanyName());
-            qnaMap.put("totalPage", Qna.getTotalPages());
-            qnaMap.put("totalElement", Qna.getTotalPages());
-            qnaList.add(qnaMap);
+            Map<String, Object> companyQnaMap = new LinkedHashMap<>();
+
+            companyQnaMap.put("qn_id", entity.getQnId());
+            companyQnaMap.put("qn_type", entity.getQnType());
+            companyQnaMap.put("qn_userid", entity.getUserInfo().getUserid());
+            companyQnaMap.put("qn_title", entity.getQnTitle());
+            companyQnaMap.put("qn_created", entity.getQnCreated());
+            companyQnaMap.put("qn_state", entity.getQnState());
+            companyQnaMap.put("product_name", entity.getProduct().getProductName());
+            companyQnaMap.put("totalPage", checkQna.getTotalPages());
+            companyQnaMap.put("totalElement", checkQna.getTotalPages());
+
+            qnaList.add(companyQnaMap);
+
         });
 
         return new ResQnaDto(qnaList);
@@ -123,13 +163,13 @@ public class QnaServiceImpl implements QnaService {
     /* dto qSort 0 = 사이트, 1 = 가게 */
     @Transactional
     @Override
-    public ResResultDto qnaWrite(UserInfo userInfo, ReqQnaDto reqQnaDto, Long c_id) {
+    public ResResultDto qnaWrite(UserInfo userInfo, ReqQnaDto reqQnaDto, Long p_id) {
+
+        UserInfo checkUserInfo = userRepository.findById(userInfo.getId())
+                .orElseThrow(() -> new CustomException.ResourceNotFoundException("회원 정보를 찾을 수 없습니다."));
 
         /* 사이트 문의사항 작성 */
-        if (reqQnaDto.getQnaSort() == 0 && c_id == null) {
-
-            UserInfo checkUserInfo = userRepository.findById(userInfo.getId())
-                    .orElseThrow(() -> new CustomException.ResourceNotFoundException("회원 정보를 찾을 수 없습니다."));
+        if (reqQnaDto.getQnaSort() == 0 && p_id == null) {
 
             Qna qna = qnaRepository.save(
                     Qna.builder()
@@ -145,11 +185,11 @@ public class QnaServiceImpl implements QnaService {
             return new ResResultDto(qna.getQnId(), "사이트 문의사항을 작성 했습니다.");
         }
 
-        /* 가게 문의사항 작성 */
-        UserInfo checkUserInfo = userRepository.findById(userInfo.getId())
-                .orElseThrow(() -> new CustomException.ResourceNotFoundException("회원 정보를 찾을 수 없습니다."));
+        /* 상품 문의사항 작성 */
+        Product checkProduct = productRepository.findById(p_id)
+                .orElseThrow(() -> new CustomException.ResourceNotFoundException("상품을 찾을 수 없습니다."));
 
-        CompanyInfo checkCompanyInfo = companyRepository.findById(c_id)
+        CompanyInfo checkCompanyInfo = companyRepository.findById(checkProduct.getProductCompanyId().getCompanyId())
                 .orElseThrow(() -> new CustomException.ResourceNotFoundException("가게 정보를 찾을 수 없습니다."));
 
         Qna qna = qnaRepository.save(
@@ -161,22 +201,53 @@ public class QnaServiceImpl implements QnaService {
                         .qnContent(reqQnaDto.getQnaContent())
                         .userInfo(checkUserInfo)
                         .companyInfo(checkCompanyInfo)
+                        .product(checkProduct)
                         .build()
         );
 
-        return new ResResultDto(qna.getQnId(), "가게 문의사항을 작성 했습니다.");
+        return new ResResultDto(qna.getQnId(), "상품 문의사항을 작성 했습니다.");
     }
     
     /* 문의사항 수정 */
     @Transactional
     @Override
-    public ResResultDto qnaEdit(UserInfo userInfo, ReqQnaDto reqQnaDto, Long qn_id) {
+    public ResResultDto qnaEdit(UserInfo userInfo, ReqQnaDto reqQnaDto, Long p_id, Long qn_id) {
 
         UserInfo checkUserInfo = userRepository.findById(userInfo.getId())
                 .orElseThrow(() -> new CustomException.ResourceNotFoundException("회원 정보를 찾을 수 없습니다."));
 
-        Qna checkQna = qnaRepository.findById(qn_id)
+        /* 사이트 문의사항 수정 */
+        if(p_id == null) {
+
+            Qna checkQna = qnaRepository.findById(qn_id)
+                    .orElseThrow(() -> new CustomException.ResourceNotFoundException("문의사항을 찾을 수 없습니다."));
+
+            checkQna = qnaRepository.save(
+                    Qna.builder()
+                            .qnId(checkQna.getQnId())
+                            .qnSort(checkQna.getQnSort())
+                            .qnType(reqQnaDto.getQnaType())
+                            .qnTitle(reqQnaDto.getQnaTitle())
+                            .qnContent(reqQnaDto.getQnaContent())
+                            .qnWriter(checkUserInfo.getUserid())
+                            .qnCreated(checkQna.getQnCreated())
+                            .qnDeleted(checkQna.getQnDeleted())
+                            .userInfo(checkUserInfo)
+                            .build()
+            );
+
+            return new ResResultDto(checkQna.getQnId(), "사이트 문의사항을 수정 했습니다.");
+        }
+
+        /* 상품 문의사항 수정 */
+        Product checkProduct = productRepository.findById(p_id)
+                .orElseThrow(() -> new CustomException.ResourceNotFoundException("상품을 찾을 수 없습니다."));
+
+        Qna checkQna = qnaRepository.findByProductAndQnId(checkProduct, qn_id)
                 .orElseThrow(() -> new CustomException.ResourceNotFoundException("문의사항을 찾을 수 없습니다."));
+
+        CompanyInfo checkCompanyInfo = companyRepository.findById(checkProduct.getProductCompanyId().getCompanyId())
+                .orElseThrow(() -> new CustomException.ResourceNotFoundException("가게 정보를 찾을 수 없습니다."));
 
         checkQna = qnaRepository.save(
                 Qna.builder()
@@ -190,21 +261,39 @@ public class QnaServiceImpl implements QnaService {
                         .qnDeleted(checkQna.getQnDeleted())
                         .userInfo(checkUserInfo)
                         .companyInfo(checkQna.getCompanyInfo())
+                        .product(checkProduct)
                         .build()
         );
 
-        return new ResResultDto(checkQna.getQnId(), "문의사항을 수정 했습니다.");
+        return new ResResultDto(checkQna.getQnId(),"상품 문의사항을 수정 했습니다.");
+
     }
 
     /* 문의사항 삭제 상태 변경 */
     @Transactional
     @Override
-    public ResResultDto qnaDelete(UserInfo userInfo, Long qn_id) {
+    public ResResultDto qnaDelete(UserInfo userInfo, Long p_id, Long qn_id) {
 
         UserInfo checkUserInfo = userRepository.findById(userInfo.getId())
                 .orElseThrow(() -> new CustomException.ResourceNotFoundException("회원 정보를 찾을 수 없습니다."));
 
-        Qna checkQna = qnaRepository.findById(qn_id)
+        /* 사이트 문의사항 삭제 */
+        if(p_id == null) {
+
+            Qna checkQna = qnaRepository.findById(qn_id)
+                    .orElseThrow(() -> new CustomException.ResourceNotFoundException("문의사항을 찾을 수 없습니다."));
+
+            /* 문의사항 삭제 상태 변경 (삭제) */
+            qnaRepository.updateQnADeleted(checkQna.getQnId());
+
+            return new ResResultDto(checkQna.getQnId(),"문의사항을 삭제 했습니다.");
+        }
+
+        /* 상품 문의사항 삭제 */
+        Product checkProduct = productRepository.findById(p_id)
+                .orElseThrow(() -> new CustomException.ResourceNotFoundException("상품을 찾을 수 없습니다."));
+
+        Qna checkQna = qnaRepository.findByProductAndQnId(checkProduct, qn_id)
                 .orElseThrow(() -> new CustomException.ResourceNotFoundException("문의사항을 찾을 수 없습니다."));
 
         /* 문의사항 삭제 상태 변경 (삭제) */
