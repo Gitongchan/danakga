f78906083198fe218f1148c35c9493fe9e5f58d9
Jinny <ppwm111@naver.com>
2022-11-09 17:32:09 +0900

    refactor: user부분 변경 중 #344

--- src/main/resources/static/js/user/myPageActivities.js
+++ src/main/resources/static/js/user/myPageActivities.js
@@ -1,245 +1,224 @@
-//data[0].totalElement //총 개수
-//data[0].totalPage // 페이지 그룹 개수
-// 이전 버튼 : 화면에 그려진 첫번째 페이지 - 1
-// 다음 버튼 : 화면에 그려진 마지막 페이지 + 1
-const pagenation = document.querySelector('.mypage_article .text-center .pagination');
-const myBoardList = document.getElementById('myBoardTableList');
+//내가 작성한 게시글
+const myBoardBtn = document.getElementById('my_board_search');
+//내가 댓글을 남긴 게시글
+const myCommentBoardBtn = document.getElementById('my_commentBoard_search');
+//내 댓글
+const myCommentBtn = document.getElementById('my_comment_search');
 
-function freeRender(url) {
-    fetch(`${url}page=0`)
-        .then((res) => res.json())
-        .then((data) => {
-            pagenation.innerHTML='';
-            renderFreePagination(data.lists[0].totalPage,url);
-        })
-}
+const cateValue = document.getElementById('category_value');
+const freeBtn = document.getElementById('free');
 
-function qnaRender(url) {
-    fetch(`${url}page=0`)
-        .then((res) => res.json())
-        .then((data) => {
-            pagenation.innerHTML='';
-            renderQnaPagination(data.comments[0].totalPage,url);
-        })
-}
+const qnaBtn = document.getElementById('qna');
 
-function renderFreePagination(currentPage,url) {
-    fetch(`${url}page=0`)
-        .then((res)=>res.json())
-        .then((data)=>{
-            //총 페이지 수
-            const total = Math.ceil(data.lists[0].totalElement/10);
-            //화면에 보여질 페이지 그룹
-            const group = Math.ceil(currentPage/10);
+//table에 값넣기
+const tableList = document.getElementById("myBoardTableList");
 
-            //마지막 번호
-            let last = group * 10;
-            //만약 마지막 번호가 총 갯수보다 많다면 마지막 번호는 총 개수로
-            if (last > total) last = total;
-            let first = last - (10 - 1) <= 0 ? 1 : last - (10 - 1);
-            let next = last + 1;
-            let prev = first - 1;
+const li_board = document.getElementById("li_board-search");
+const li_commentBoard = document.getElementById("li-comment-board-search");
+const li_comment = document.getElementById("li-comment-search");
+const li_free = document.getElementById('free');
+const li_qna = document.getElementById('qna');
 
-            // console.log(`총 페이지 수 : ${total}`);
-            // console.log(`보여질 페이지 그룹 : ${group}`);
-            // console.log(`첫번째 페이지 : ${first}`);
-            // console.log(`마지막 페이지 : ${last}`);
-            // console.log(`다음 페이지 : ${next}`);
-            // console.log(`이전 페이지 : ${prev}`);
+myBoardBtn.addEventListener('click', (event) => {
+    tableList.innerHTML = '';
+    cateValue.value = "myboard"
+    pagenation.innerHTML='';
+    li_qna.classList.remove('active');
+    li_free.classList.remove('active');
+    li_commentBoard.classList.remove('select');
+    li_comment.classList.remove('select');
+    event.target.parentElement.classList.add("select");
+})
 
-            const fragmentPage = document.createDocumentFragment();
-            if (prev > 0) {
-                const allpreli = document.createElement('li');
-                allpreli.classList.add('page-item');
-                allpreli.insertAdjacentHTML("beforeend", `<a class="page-link" id='allprev'>&lt;&lt;</a>`);
+myCommentBoardBtn.addEventListener('click',  (event) => {
+    tableList.innerHTML = '';
+    cateValue.value = "commentBoard"
+    pagenation.innerHTML='';
+    li_qna.classList.remove('active');
+    li_free.classList.remove('active');
+    li_board.classList.remove('select');
+    li_comment.classList.remove('select');
+    event.target.parentElement.classList.add("select");
 
-                const preli = document.createElement('li');
-                preli.insertAdjacentHTML("beforeend", `<a class="page-link" id='prev'>&lt;</a>`);
-                preli.classList.add("page-item")
+})
 
-                fragmentPage.appendChild(allpreli);
-                fragmentPage.appendChild(preli);
-            }
+myCommentBtn.addEventListener('click',  (event) => {
+    tableList.innerHTML = '';
+    cateValue.value = "comment"
+    pagenation.innerHTML='';
+    li_qna.classList.remove('active');
+    li_free.classList.remove('active');
+    li_board.classList.remove("select");
+    li_commentBoard.classList.remove("select");
+    event.target.parentElement.classList.add("select");
 
-            for (let i = first; i <= last; i++) {
-                const li = document.createElement("li");
-                li.insertAdjacentHTML("beforeend", `<a class="page-link" id='${i-1}'>${i}</a>`);
-                li.classList.add("page-item")
+})
 
-                fragmentPage.appendChild(li);
-            }
+freeBtn.addEventListener('click', async ()=>{
+    li_qna.classList.remove('active');
+    li_free.classList.add('active');
+    if(cateValue.value === 'myboard'){
+        try{
+            console.log(cateValue.value);
+            // 내가 작성한 게시글 찾기 이벤트
+            const res = await fetch(`/api/user/myPostList/자유게시판?page=0`);
 
-            if (last < total) {
-                const allendli = document.createElement('li');
-                allendli.classList.add("page-item")
-                allendli.insertAdjacentHTML("beforeend", `<a class="page-link"  id='allnext'>&gt;&gt;</a>`);
+            if(res.ok){
+                const data = await res.json();
+                tableList.innerHTML = '';
 
-                const endli = document.createElement('li');
-                endli.classList.add("page-item");
-                endli.insertAdjacentHTML("beforeend", `<a  class="page-link" id='next'>&gt;</a>`);
-
-                fragmentPage.appendChild(endli);
-                fragmentPage.appendChild(allendli);
-            }
-
-            pagenation.appendChild(fragmentPage);
-            // 페이지 목록 생성
-
-            // document.querySelector(".text-center .pagination li a").classList.remove("active");
-            // document.querySelector(`.text-center .pagination li a#page-${currentPage}`).classList.add("active");
-
-            const setnum = document.querySelectorAll(".text-center .pagination li a")
-            for (const id of setnum) {
-                id.addEventListener('click', function(e) {
-                    e.preventDefault();
-                    let item = e.target;
-                    let id = item.id;
-                    let selectedPage = item.innerText;
-
-                    if (id === "next") selectedPage = next;
-                    else if (id === "prev") selectedPage = prev;
-                    else if (id === "allprev") selectedPage = 1;
-                    else if (id === "allnext") selectedPage = total;
-                    //페이지 그리는 함수
-                    else onFreeList(id,url);
-
-
-
-                    pagenation.innerHTML = "";
-                    renderFreePagination(selectedPage,url);//페이지네이션 그리는 함수
-
-                })
-            }
-        })
-}
-
-
-function renderQnaPagination(currentPage,url) {
-    fetch(`${url}page=0`)
-        .then((res)=>res.json())
-        .then((data)=>{
-            //총 페이지 수
-            const total = Math.ceil(data.comments[0].totalElement/10);
-            //화면에 보여질 페이지 그룹
-            const group = Math.ceil(currentPage/10);
-
-            //마지막 번호
-            let last = group * 10;
-            //만약 마지막 번호가 총 갯수보다 많다면 마지막 번호는 총 개수로
-            if (last > total) last = total;
-            let first = last - (10 - 1) <= 0 ? 1 : last - (10 - 1);
-            let next = last + 1;
-            let prev = first - 1;
-
-            // console.log(`총 페이지 수 : ${total}`);
-            // console.log(`보여질 페이지 그룹 : ${group}`);
-            // console.log(`첫번째 페이지 : ${first}`);
-            // console.log(`마지막 페이지 : ${last}`);
-            // console.log(`다음 페이지 : ${next}`);
-            // console.log(`이전 페이지 : ${prev}`);
-
-            const fragmentPage = document.createDocumentFragment();
-            if (prev > 0) {
-                const allpreli = document.createElement('li');
-                allpreli.classList.add('page-item');
-                allpreli.insertAdjacentHTML("beforeend", `<a class="page-link" id='allprev'>&lt;&lt;</a>`);
-
-                const preli = document.createElement('li');
-                preli.insertAdjacentHTML("beforeend", `<a class="page-link" id='prev'>&lt;</a>`);
-                preli.classList.add("page-item")
-
-                fragmentPage.appendChild(allpreli);
-                fragmentPage.appendChild(preli);
-            }
-
-            for (let i = first; i <= last; i++) {
-                const li = document.createElement("li");
-                li.insertAdjacentHTML("beforeend", `<a class="page-link" id='${i-1}'>${i}</a>`);
-                li.classList.add("page-item")
-
-                fragmentPage.appendChild(li);
-            }
-
-            if (last < total) {
-                const allendli = document.createElement('li');
-                allendli.classList.add("page-item")
-                allendli.insertAdjacentHTML("beforeend", `<a class="page-link"  id='allnext'>&gt;&gt;</a>`);
-
-                const endli = document.createElement('li');
-                endli.classList.add("page-item");
-                endli.insertAdjacentHTML("beforeend", `<a  class="page-link" id='next'>&gt;</a>`);
-
-                fragmentPage.appendChild(endli);
-                fragmentPage.appendChild(allendli);
-            }
-
-            pagenation.appendChild(fragmentPage);
-            // 페이지 목록 생성
-
-            // document.querySelector(".text-center .pagination li a").classList.remove("active");
-            // document.querySelector(`.text-center .pagination li a#page-${currentPage}`).classList.add("active");
-
-            const setnum = document.querySelectorAll(".text-center .pagination li a")
-            for (const id of setnum) {
-                id.addEventListener('click', function(e) {
-                    e.preventDefault();
-                    let item = e.target;
-                    let id = item.id;
-                    let selectedPage = item.innerText;
-
-                    if (id === "next") selectedPage = next;
-                    else if (id === "prev") selectedPage = prev;
-                    else if (id === "allprev") selectedPage = 1;
-                    else if (id === "allnext") selectedPage = total;
-                    //페이지 그리는 함수
-                    else onQnaList(id,url);
-
-
-
-                    pagenation.innerHTML = "";
-                    renderQnaPagination(selectedPage,url);//페이지네이션 그리는 함수
-
-                })
-            }
-        })
-}
-
-//페이지 번호 클릭 시 이동하는 곳
-function onFreeList(e,url){
-    fetch(`${url}page=${e}`)
-        .then((res)=>res.json())
-        .then((data)=>{
-            myBoardList.innerHTML="";
-            for(let i=0; i<data.lists.length; i++){
-                console.log(data.lists[i]);
-                const tr = document.createElement('tr');
-                tr.innerHTML =
-                    `<td>${data.lists[i].bd_id}</td>
+                for(let i=0; i<data.lists.length; i++){
+                    const tr = document.createElement('tr');
+                    tr.innerHTML =
+                        `<td>${data.lists[i].bd_id}</td>
                  <td><a href="/board/info?boardid=${data.lists[i].bd_id}?bdwriter=${data.lists[i].bd_writer}">${data.lists[i].bd_title}</a></td>
                  <td>${data.lists[i].bd_writer}</td>
                  <td>${data.lists[i].bd_created.split('.')[0]}</td>
                  <td>${data.lists[i].bd_views}</td>`
 
-                myBoardList.appendChild(tr);
+                    tableList.appendChild(tr);
+                }
+                freeRender('/api/user/myPostList/자유게시판?');
             }
-        })
-}
+        }catch (e) {
 
-function onQnaList(e,url){
-    fetch(`${url}page=${e}`)
-        .then((res)=>res.json())
-        .then((data)=>{
-            myBoardList.innerHTML="";
-            for(let i=0; i<data.comments.length; i++){
-                const tr = document.createElement('tr');
-                tr.innerHTML =
-                    `<td>${data.comments[i].bd_id}</td>
+        }
+    } else if(cateValue.value === 'commentBoard'){
+        // 내가 댓글을 남긴 게시글 찾기 이벤트
+        try{
+            console.log(cateValue.value);
+
+            const res = await fetch(`/api/user/myCommentsPost/자유게시판?page=0`);
+            if(res.ok){
+                const data = await res.json();
+                tableList.innerHTML = '';
+
+                for(let i=0; i<data.lists.length; i++){
+                    const tr = document.createElement('tr');
+                    tr.innerHTML =
+                        `<td>${data.lists[i].bd_id}</td>
+                 <td><a href="/board/info?boardid=${data.lists[i].bd_id}?bdwriter=${data.lists[i].bd_writer}">${data.lists[i].bd_title}</a></td>
+                 <td>${data.lists[i].bd_writer}</td>
+                 <td>${data.lists[i].bd_created.split('.')[0]}</td>
+                 <td>${data.lists[i].bd_views}</td>`
+
+                    tableList.appendChild(tr);
+                }
+                freeRender('/api/user/myCommentsPost/자유게시판?');
+            }
+        }catch (e) {
+
+        }
+    } else if(cateValue.value === 'comment'){
+        // 내가 작성한 댓글 찾기 이벤트
+        try{
+            console.log(cateValue.value);
+            const res = await fetch(`/api/user/myCommentsList/자유게시판?page=0`);
+
+            if(res.ok){
+                const data = await res.json();
+                tableList.innerHTML = '';
+
+                for(let i=0; i<data.comments.length; i++){
+                    const tr = document.createElement('tr');
+                    tr.innerHTML =
+                        `<td>${data.comments[i].bd_id}</td>
                  <td><a href="/board/info?boardid=${data.comments[i].bd_id}">${data.comments[i].cm_content}</a></td>
                  <td>${data.comments[i].cm_writer}</td>
                  <td>${data.comments[i].cm_created.split('.')[0]}</td>
                  <td>${data.comments[i].cm_modify}</td>`
 
-                myBoardList.appendChild(tr);
+                    tableList.appendChild(tr);
+                }
+                qnaRender('/api/user/myCommentsList/자유게시판?');
             }
-        })
-}
+        }catch (e) {
+
+        }
+    }
+})
+
+qnaBtn.addEventListener('click',async ()=>{
+    li_free.classList.remove('active');
+    li_qna.classList.add('active');
+
+    if(cateValue.value === 'myboard'){
+        try{
+            console.log(cateValue.value);
+            // 내가 작성한 게시글 찾기 이벤트
+            const res = await fetch(`/api/user/myPostList/정보게시판?page=0`);
+
+            if(res.ok){
+                const data = await res.json();
+                tableList.innerHTML = '';
+
+                for(let i=0; i<data.lists.length; i++){
+                    const tr = document.createElement('tr');
+                    tr.innerHTML =
+                        `<td>${data.lists[i].bd_id}</td>
+                 <td><a href="/board/info?boardid=${data.lists[i].bd_id}?bdwriter=${data.lists[i].bd_writer}">${data.lists[i].bd_title}</a></td>
+                 <td>${data.lists[i].bd_writer}</td>
+                 <td>${data.lists[i].bd_created.split('.')[0]}</td>
+                 <td>${data.lists[i].bd_views}</td>`
+
+                    tableList.appendChild(tr);
+                }
+                freeRender('/api/user/myPostList/정보게시판?');
+            }
+        }catch (e) {
+
+        }
+    } else if(cateValue.value === 'commentBoard'){
+        // 내가 댓글을 남긴 게시글 찾기 이벤트
+        try{
+            console.log(cateValue.value);
+
+            const res = await fetch(`/api/user/myCommentsPost/정보게시판?page=0`);
+            if(res.ok){
+                const data = await res.json();
+                tableList.innerHTML = '';
+
+                for(let i=0; i<data.lists.length; i++){
+                    const tr = document.createElement('tr');
+                    tr.innerHTML =
+                        `<td>${data.lists[i].bd_id}</td>
+                 <td><a href="/board/info?boardid=${data.lists[i].bd_id}?bdwriter=${data.lists[i].bd_writer}">${data.lists[i].bd_title}</a></td>
+                 <td>${data.lists[i].bd_writer}</td>
+                 <td>${data.lists[i].bd_created.split('.')[0]}</td>
+                 <td>${data.lists[i].bd_views}</td>`
+
+                    tableList.appendChild(tr);
+                }
+                freeRender('/api/user/myCommentsPost/정보게시판?');
+            }
+        }catch (e) {
+
+        }
+    } else if(cateValue.value === 'comment'){
+        // 내가 작성한 댓글 찾기 이벤트
+        try{
+            console.log(cateValue.value);
+            const res = await fetch(`/api/user/myCommentsList/정보게시판?page=0`);
+
+            if(res.ok){
+                const data = await res.json();
+                tableList.innerHTML = '';
+
+                for(let i=0; i<data.comments.length; i++){
+                    const tr = document.createElement('tr');
+                    tr.innerHTML =
+                        `<td>${data.comments[i].bd_id}</td>
+                 <td><a href="/board/info?boardid=${data.comments[i].bd_id}">${data.comments[i].cm_content}</a></td>
+                 <td>${data.comments[i].cm_writer}</td>
+                 <td>${data.comments[i].cm_created.split('.')[0]}</td>
+                 <td>${data.comments[i].cm_modify}</td>`
+
+                    tableList.appendChild(tr);
+                }
+                qnaRender('/api/user/myCommentsList/정보게시판?')
+            }
+        }catch (e) {
+
+        }
+    }
+})
