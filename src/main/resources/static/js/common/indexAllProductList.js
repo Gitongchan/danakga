a7062b3ca8f0a212a8cafde23730a1cbcb2a84d0
Jinny <ppwm111@naver.com>
2022-11-10 17:30:34 +0900

    feat: 메인이미지 넣기 #355

--- src/main/resources/static/js/common/indexAllProductList.js
+++ src/main/resources/static/js/common/indexAllProductList.js
@@ -1,40 +1,13 @@
 const orderItem = document.getElementById('orderd-item');
-const indexsmall = document.getElementById('index_small');
 const orderCount = document.getElementById('order-count');
 const uploadCount = document.getElementById('upload-count');
 const viewCount = document.getElementById('view-count');
 const manyView = document.getElementById('many-view');
+
     //업로드 일 순
     fetch('/api/product/main-page/list/productUploadDate?page=0')
         .then((res) => res.json())
         .then((data) => {
-            for(let u =0;  u < data.length-1; u++){
-                let con = document.getElementById(`ss${u}`);
-                con.innerHTML =
-                    `<img src="${data[u].productPhoto}"/>
-                    <div class="content">
-                        <h2><span>신규등록</span>
-                            ${data[u].productName}
-                        </h2>
-                        <p></p>
-                        <h3><span>가격 :</span>${data[u].productPrice}원</h3>
-                        <div class="button">
-                            <a href="/product/info?productId=${data[u].productId}" class="btn" id="order-date-a-0">상세정보</a>
-                        </div>
-                    </div>
-                `
-            }
-            indexsmall.innerHTML =`
-            <img src=${data[7].productPhoto} id="small_img" alt="#"/>
-                <div class="content">
-                    <h2>
-                        <span>신규</span>
-                        ${data[7].productName}
-                    </h2>
-                    <h3>${data[7].productPrice}원</h3>
-                </div>
-              `
-
             uploadCount.innerHTML+= `<h4 class="list-title">신규 상품 순</h4>`;
             for(let u1=0; u1<3; u1++){
                 uploadCount.innerHTML+=` 
@@ -52,8 +25,6 @@ const manyView = document.getElementById('many-view');
                         </div>
                     </div>`
             }
-    
-    
         })
 
     //주문순
